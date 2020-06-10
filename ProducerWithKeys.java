bbeca617e98554dfc7a39297c5e4cd88c13f1be8
vj98 <bhativijay17@gmail.com>
2020-06-10 22:08:19 +0530

    Added file for producer with keys

---
+++ ProducerWithKeys.java
@@ -0,0 +1,70 @@
+package com.vijay.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerWithKeys {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        String bootstrapServers = "127.0.0.1:9092";
+
+        final Logger logger = LoggerFactory.getLogger(ProducerCallBack.class);
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        // How will you send the data and how do you serialize the bytes
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            // create a producer record
+
+            String topic = "first_topic";
+            String value = "hello world " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            logger.info("Key: " + key);
+
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            // send data asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // Excecutes every time a record is successfully send or an exception is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    }
+                    else {
+                        logger.error("Error While Producing ", e);
+                    }
+                }
+            }).get(); // block the .send() 
+        }
+        // Send data
+        // producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush data and close
+        producer.close();
+
+        // CLI for consumer
+        // bin/kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic first_topic --group my-third-application
+    }
+}
